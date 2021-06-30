787aa6ecadd5f54839720e54d26cbad72d8955d3
Marcos Augusto <augustoprofemp@gmail.com>
2021-06-30 17:29:40 -0300

    feat: creating tui card header
    - improving create tweet component
    - improving create tweet footer
    - using tui card in other components

--- libs/tweeter-ui/src/lib/card/card.module.ts
+++ libs/tweeter-ui/src/lib/card/card.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { CardComponent } from './card.component';
 import { MatCardModule } from '@angular/material/card';
 import { CardFooterComponent } from './card-footer/card-footer.component';
+import { CardHeaderComponent } from './card-header/card-header.component';
 
 @NgModule({
-  declarations: [CardComponent, CardFooterComponent],
+  declarations: [CardComponent, CardFooterComponent, CardHeaderComponent],
   imports: [CommonModule, MatCardModule],
-  exports: [CardComponent, MatCardModule, CardFooterComponent],
+  exports: [CardComponent, MatCardModule, CardFooterComponent, CardHeaderComponent],
 })
 export class CardModule {}
