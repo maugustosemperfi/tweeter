bbf7f0b2f1c4ed0e2f5708b5f0a7e274af461265
Marcos Augusto <augustoprofemp@gmail.com>
2021-06-30 16:12:14 -0300

    feat: creating tweet component for feed module

--- apps/feed/src/app/modules/feed/feed.module.ts
+++ apps/feed/src/app/modules/feed/feed.module.ts
@@ -5,11 +5,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { FeedRoutingModule } from './feed-routing.module';
 import { FeedComponent } from './feed.component';
 import { TweetCardModule } from '@tweeter/ui';
+import { TweetComponent } from './components/tweet/tweet.component';
 
 const routes: Routes = [{ path: '', component: FeedComponent }];
 
 @NgModule({
-  declarations: [FeedComponent],
+  declarations: [FeedComponent, TweetComponent],
   imports: [
     CommonModule,
     FeedRoutingModule,
