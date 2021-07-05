9916423e3d7e6aa8369c98018405eff7b98f7adc
Marcos Augusto <augustoprofemp@gmail.com>
2021-07-05 19:11:25 -0300

    feat: adding webpack module federation to tweeter application
    - adding micro frontends configurations to feed and profile projetc

--- apps/feed/src/app/app.module.ts
+++ apps/feed/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { RouterModule } from '@angular/router';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { loadRemoteModule } from '@angular-architects/module-federation';
 
 @NgModule({
   declarations: [AppComponent],
@@ -16,6 +17,15 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
           path: '',
           loadChildren: () =>
             import('./modules/feed/feed.module').then((m) => m.FeedModule),
+        },
+        {
+          path: 'profile',
+          loadChildren: () =>
+            loadRemoteModule({
+              remoteEntry: 'http://localhost:4201/remoteEntry.js',
+              remoteName: 'profile',
+              exposedModule: './Module',
+            }).then((m) => m.ProfileModule),
         },
       ],
       { initialNavigation: 'enabled' }
