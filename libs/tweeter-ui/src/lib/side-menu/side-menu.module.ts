cac0762ce82aab94fcd8561d3e5255f956c46b41
Marcos Augusto <augustoprofemp@gmail.com>
2021-07-05 19:32:08 -0300

    feat: adding routerlink to side menu component

--- libs/tweeter-ui/src/lib/side-menu/side-menu.module.ts
+++ libs/tweeter-ui/src/lib/side-menu/side-menu.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { SideMenuComponent } from './side-menu.component';
 import { MatListModule } from '@angular/material/list';
 import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [SideMenuComponent],
-  imports: [CommonModule, MatListModule, MatIconModule],
+  imports: [CommonModule, MatListModule, MatIconModule, RouterModule],
   exports: [SideMenuComponent],
 })
 export class SideMenuModule {}
